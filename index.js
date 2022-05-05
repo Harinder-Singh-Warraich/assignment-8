2cf008ac624edcda0e5cd3398462b654a1c46b05
Harinder-Singh-Warraich <75014420+Harinder-Singh-Warraich@users.noreply.github.com>
2022-05-05 23:41:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const mongoose=require('mongoose');
 const path=require('path');
 const Data=require('./models/dat');
 const methodOverride=require('method-override');
-if(process.env.NODE_ENV !=="production"){
-    require('dotenv').config();
-}
 
 
-mongoose.connect(process.env.url)
+
+mongoose.connect('mongodb://localhost:27017/reception-db')
 .then(()=>{
     console.log("DB CONNECTED")
 })
@@ -23,9 +21,6 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')))
 
 
-app.get('/',(req,res)=>{
-    res.redirect('/home');
-})
 app.get('/home',async (req,res)=>{
     const data=await Data.find({});
     res.render('home',{data});
@@ -34,7 +29,10 @@ app.get('/home/enter',(req,res)=>{
     res.render('enter');
 })
 app.post('/home',async(req,res)=>{
-    const {name,email,phone,cinh,cinm}=req.body;
+    const {name,email,phone}=req.body;
+    let date=new Date();
+    let cinh=date.getHours();
+    let cinm=date.getMinutes();
     sendemail(email,cinh,cinm);
     await Data.create({name,email,phone,cinh,cinm});
     res.redirect('/home');
@@ -46,7 +44,9 @@ app.get('/home/:id',async(req,res)=>{
 })
 app.put('/home/:id',async(req,res)=>{
     const {id}=req.params;
-    const {couth,coutm}=req.body;
+    let date2=new Date();
+    let couth=date2.getHours();
+    let coutm=date2.getMinutes();
     const oh=couth;
     const om=coutm;
     const d=await Data.findById(id)
@@ -63,45 +63,34 @@ app.delete('/home/:id',async (req,res)=>{
 
 function sendemail(email,cinh,cinm){
     const sgMail=require('@sendgrid/mail');
-  sgMail.setApiKey(process.env.api);
-  let m=cinm.toString();
-  let h=cinh.toString();;
-  if(cinm<=9){
-      m='0'+cinm.toString();
-  }
-  if(cinh<=9){
-      h='0'+cinh.toString();
-  }
+   const  sendgrid='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';
+  sgMail.setApiKey(sendgrid);
   const msg={
       to: email,
       from: "harinder1414.cse19@chitkara.edu.in",
       subject:"Entering building",
-      text:`Hi you entered the building at ${h}:${m}`
+      text:`Hi you entered the building at ${cinh}:${cinm}`
   };
-  sgMail.send(msg);
+  sgMail.send(msg)
+  .then((res)=>console.log("SENT"))
+  .catch((error)=>console.log(error.message))
 }
-
 
 function sendexmail(email,couth,coutm){
     const sgMail=require('@sendgrid/mail');
-  sgMail.setApiKey(process.env.api);
-  let m=coutm.toString();
-  let h=couth.toString();
-  if(coutm<=9){
-      m='0'+coutm.toString();
-  }
-  if(couth<=9){
-      h='0'+couth.toString()
-  }
+   const  sendgrid='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';
+  sgMail.setApiKey(sendgrid);
   const msg={
       to: email,
       from: "harinder1414.cse19@chitkara.edu.in",
       subject:"Checking out",
-      text:`Hi you checked out at ${h}:${m}`
+      text:`Hi you checked out at ${couth}:${coutm}`
   };
-  sgMail.send(msg);
+  sgMail.send(msg)
+  .then((res)=>console.log("SENT"))
+  .catch((error)=>console.log(error.message))
 }
 
-app.listen(process.env.PORT || 3000,(req,res)=>{
-    console.log("UP AT 3000");
+app.listen(2323,(req,res)=>{
+    console.log("UP AT 2323");
 })
